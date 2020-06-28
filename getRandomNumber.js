e6454d783f40df637b739b8e7c27f9939e537b94
ath08 <0807ath@gmail.com>
2020-06-28 14:27:18 +0400

    called getRandomNumber function

--- getRandomNumber.js
+++ getRandomNumber.js
@@ -1,3 +1,8 @@
 const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 };
+
+console.log(getRandomNumber(5, 10));
+console.log(getRandomNumber(1, 14));
+console.log(getRandomNumber(50, 100));
+console.log(getRandomNumber(-5, 0));
