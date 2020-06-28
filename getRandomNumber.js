191a5ee8cff922e2174bc1b69c665050145978bc
ath08 <0807ath@gmail.com>
2020-06-28 15:01:01 +0400

    Return each files to its place

--- getRandomNumber.js
+++ getRandomNumber.js
@@ -1,12 +1,11 @@
-const getRandomLetter = () => {
-    const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-       
-    return randomChars.charAt(Math.floor(Math.random() * randomChars.length));
-    
-}
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+};
 
-console.log(getRandomLetter());
-console.log(getRandomLetter());
-console.log(getRandomLetter());
-console.log(getRandomLetter());
-console.log(getRandomLetter());
+console.log(getRandomNumber(5, 10));
+console.log(getRandomNumber(1, 14));
+console.log(getRandomNumber(50, 100));
+console.log(getRandomNumber(-5, 0));
+console.log(getRandomNumber(0, 9));
+console.log(getRandomNumber(-5, 5));
+
